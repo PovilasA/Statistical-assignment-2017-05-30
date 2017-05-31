1d578e3bce4faa4a4720fa8449befa9d40ad4a36
PovilasA <povilas.asijavicius@gmail.com>
2017-05-31 16:14:57 +0300

    Read data some modifications

--- read_data.R
+++ read_data.R
@@ -6,10 +6,7 @@ data_structure = read.csv("data_structure.csv")
 dates_structure = list(
   "due_date" = "%m/%d/%Y",
   "first_status_day_date" = "%m/%d/%Y",
-  "first_status_time_of_day" = "%m/%d/%Y %H:%M:%S",
-  "first_status" = "%m/%d/%Y %H:%M:%S",
   "paid_date" = "%m/%d/%Y",
-  "arrived_date" = "%m/%d/%Y %H:%M",
   "Variable_42" = "%m/%d/%Y",
   "Variable_43" = "%m/%d/%Y",
   "Variable_44" = "%m/%d/%Y"
@@ -19,12 +16,22 @@ dates_structure = list(
 # for analysis but combinations of those two variables might be useful as well.
 data[["first_status"]] = paste(data[["first_status_day_date"]],
                                data[["first_status_time_of_day"]])
+data[["first_status"]] = as.POSIXct(strptime(data[["first_status"]],
+                                             "%m/%d/%Y %H:%M:%S"))
 
 # Valid time format for specific variable. We add random date next to the time.
-data[["first_status_time_of_day"]] = paste('2017-06-01', 
+data[["first_status_time_of_day"]] = paste('01/06/2017', 
                                            data[["first_status_time_of_day"]])
+data[["first_status_time_of_day"]] = as.POSIXct(strptime(
+                                      data[["first_status_time_of_day"]],
+                                      "%m/%d/%Y %H:%M:%S"))
 
-for (name in setdiff(names(data), "first_status")) {
+# Fix one more time variables
+data[["arrived_date"]] = as.POSIXct(strptime(data[["arrived_date"]],
+                                             "%m/%d/%Y %H:%M"))
+
+metrics = c("first_status","first_status_time_of_day","arrived_date")
+for (name in setdiff(names(data), metrics)) {
   if (data_structure[[name]] %in% c('Ordinal','Categorical','Binary','ID')) {
     data[[name]] = as.factor(data[[name]])  
   } 

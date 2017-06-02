7f21c31a2ea92b488003d2e07cea29a826fa7d12
PovilasA <povilas.asijavicius@gmail.com>
2017-06-02 12:19:41 +0300

    Select training and testing data for modeling

---
+++ modeling.R
@@ -0,0 +1,8 @@
+# setting seed for reproducibility.
+set.seed(1)
+# Indices for training set and test set. We take 70% of the data for training.
+train_indices = sample(1:length(model_data1[,1]), length(model_data1[,1])*0.7)
+test_indices = setdiff(1:length(model_data1[,1]), train_indices)
+
+train = model_data1[train_indices,]
+test = model_data1[test_indices,]

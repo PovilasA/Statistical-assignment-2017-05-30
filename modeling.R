5e377a42b26c9bb3e7fd6ebcb0ea00edba752538
PovilasA <povilas.asijavicius@gmail.com>
2017-06-02 12:25:50 +0300

    Use boruta selected variables for modeling

--- modeling.R
+++ modeling.R
@@ -6,3 +6,8 @@ test_indices = setdiff(1:length(model_data1[,1]), train_indices)
 
 train = model_data1[train_indices,]
 test = model_data1[test_indices,]
+
+# Run procedure to select variables for modeling. This creates variables
+# 'selected_variables' (vector of selected variables) and 
+# 'formula'(formula with selected features used for modeling).
+source('boruta.R')
